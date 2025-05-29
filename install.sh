a0c79e36aa7509018e460ffb30284e93deff7fe1
John <jonathan.ulmer@ulmail.de>
2025-05-29 22:45:55 +0200

    added climate install

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
 cp beeBreeding.lua "/home/proceduraltree/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/GT_New_Horizons_2.7.3_Java_17-21/.minecraft/saves/New World/opencomputers/a8d8c67c-9f4e-4ac1-97db-86f3ae8da6c5/home"
+cp climate.lua "/home/proceduraltree/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/GT_New_Horizons_2.7.3_Java_17-21/.minecraft/saves/New World/opencomputers/a8d8c67c-9f4e-4ac1-97db-86f3ae8da6c5/home"
