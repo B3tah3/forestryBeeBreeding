92d9473792d9a43c29dd91decbf84d5a2011c30c
John <jonathan.ulmer@ulmail.de>
2025-05-31 22:58:24 +0200

    changed installer

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 rm -rf "/home/proceduraltree/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/GT_New_Horizons_2.7.3_Java_17-21/.minecraft/saves/BeeBreeding/opencomputers/18c3c0e3-2086-4f87-9390-215c5d8a85ca/home/beez/"
 mkdir "/home/proceduraltree/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/GT_New_Horizons_2.7.3_Java_17-21/.minecraft/saves/BeeBreeding/opencomputers/18c3c0e3-2086-4f87-9390-215c5d8a85ca/home/beez/"
-cp -r *  "/home/proceduraltree/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/GT_New_Horizons_2.7.3_Java_17-21/.minecraft/saves/BeeBreeding/opencomputers/18c3c0e3-2086-4f87-9390-215c5d8a85ca/home/beez/"
+cp -r *.lua  "/home/proceduraltree/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/GT_New_Horizons_2.7.3_Java_17-21/.minecraft/saves/BeeBreeding/opencomputers/18c3c0e3-2086-4f87-9390-215c5d8a85ca/home/beez/"
